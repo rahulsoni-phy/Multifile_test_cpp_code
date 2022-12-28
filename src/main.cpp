3764a77d6161bf1c944f22acaca779c28fb98378
rahulsoni-phy <rahulsoni.phy@gmail.com>
2022-12-28 10:39:32 -0500

    test multifile c++ project in VS code

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "test.hpp"
+
+int main(){
+    HelloWorld great;
+    great.publish();
+
+    return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+#include "test.hpp"
+
+void HelloWorld::publish(){
+    std::cout << "Hello World!" << std::endl;
+}
